7c825503ea99b98d6989b553a7a38ad346aeb681
NPuccZ <101108484+NPuccZ@users.noreply.github.com>
2024-09-03 14:25:50 +0200

    Update read_table.cpp
    To be modified: input file read as environment variable

--- Bilinear_Interpolation/read_table.cpp
+++ Bilinear_Interpolation/read_table.cpp
@@ -22,13 +22,16 @@ int main(int argc, char *argv[])
     }
     double CS = 0;
     cout << "I valori immessi sono i seguenti plab pd: " << plab << " " << pd << endl;
-    CS = outCS(plab, pd); 
-
+    CS = outCS(plab, pd);
 }
 
 double outCS(double plab, double pd)
 {
+    static vector<vector<double>> cs; // 2D vector to store the cross sections
+    static vector<vector<double>> cs_ordered;
+
     double CS = 0;
+    const plab_thr = 15.92; // GeV/c fare il conto
     cout << "I valori immessi sono i seguenti plab pd: " << plab << " " << pd << endl;
 
     const int npbins = 90;
@@ -43,55 +46,60 @@ double outCS(double plab, double pd)
     vector<double> pdeut; // Fill a vector with the central values of the bins
     for (int ip = 0; ip < npbins; ++ip)
     {
-        pdeut.push_back(0.5 * (pbins[ip] + pbins[ip + 1]));
+        pdeut.push_back(sqrt(pbins[ip] * pbins[ip + 1]));
     }
 
     // Apri il file utilizzando ifstream
-    ifstream file("cross-sections.txt");
-    if (!file.is_open())
+    if (cs.empty())
     {
-        cerr << "Errore: impossibile aprire il file cross-sections.txt" << endl;
-        return 1;
-    }
 
-    vector<vector<double>> cs; // 2D vector to store the cross sections
-    vector<double> p_lab_grid; // Vector to store the p_lab values
-    string line;
-    // Leggi il file riga per riga
-    while (getline(file, line))
-    {
-        stringstream ss(line);
-        double plab_value;
-        ss >> plab_value; // Estrae il primo valore della riga (p_lab)
-        p_lab_grid.push_back(plab_value);
-
-        vector<double> cross_section_row;
-        double value;
-        while (ss >> value)
+        ifstream file("cross-sections.txt");
+        if (!file.is_open())
         {
-            cross_section_row.push_back(value); // Legge tutti i valori rimanenti (cross sections)
+            cerr << "Errore: impossibile aprire il file cross-sections.txt" << endl;
+            return 1;
         }
-        cs.push_back(cross_section_row);
-    }
 
-    // order the vector p_lab_grid and accordingly the index of the cross sections
-    vector<double> p_lab_grid_ordered = p_lab_grid;
-    sort(p_lab_grid_ordered.begin(), p_lab_grid_ordered.end());
-    vector<vector<double>> cs_ordered;
-    // fill the ordered cross sections vector
-    for (size_t i = 0; i < p_lab_grid_ordered.size(); ++i)
-    {
-        vector<double> cross_section_row;
-        for (size_t j = 0; j < p_lab_grid.size(); ++j)
+        vector<double> p_lab_grid; // Vector to store the p_lab values
+        string line;
+        // Leggi il file riga per riga
+        while (getline(file, line))
         {
-            if (p_lab_grid[j] == p_lab_grid_ordered[i])
+            stringstream ss(line);
+            double plab_value;
+            ss >> plab_value; // Estrae il primo valore della riga (p_lab)
+            p_lab_grid.push_back(plab_value);
+
+            vector<double> cross_section_row;
+            double value;
+            while (ss >> value)
             {
-                cross_section_row = cs[j];
-                break;
+                cross_section_row.push_back(value); // Legge tutti i valori rimanenti (cross sections)
             }
+            cs.push_back(cross_section_row);
         }
-        cs_ordered.push_back(cross_section_row);
+
+        // order the vector p_lab_grid and accordingly the index of the cross sections
+        vector<double> p_lab_grid_ordered = p_lab_grid;
+        sort(p_lab_grid_ordered.begin(), p_lab_grid_ordered.end());
+        // fill the ordered cross sections vector
+        for (size_t i = 0; i < p_lab_grid_ordered.size(); ++i)
+        {
+            vector<double> cross_section_row;
+            for (size_t j = 0; j < p_lab_grid.size(); ++j)
+            {
+                if (p_lab_grid[j] == p_lab_grid_ordered[i])
+                {
+                    cross_section_row = cs[j];
+                    break;
+                }
+            }
+            cs_ordered.push_back(cross_section_row);
+        }
     }
+    // Ordered list
+
+    // mettere static dump per i cout
 
     // using the upper_bound function to find the index of the p_lab value in the ordered vector
     vector<double>::const_iterator index_standard;
@@ -103,7 +111,7 @@ double outCS(double plab, double pd)
     index_standard = upper_bound(pdeut.begin(), pdeut.end(), pd);
     int index_pdeut = index_standard - pdeut.begin() - 1;
     cout << "Index pdeut: " << index_pdeut << endl;
-    if (plab > 17)
+    if (plab > plab_thr)
     {
         double p_lab1 = p_lab_grid_ordered[index_plab];
         double p_lab2 = p_lab_grid_ordered[index_plab + 1];
@@ -118,7 +126,9 @@ double outCS(double plab, double pd)
         cout << "p1: " << p1 << " p2: " << p2 << endl;
         cout << "cs11: " << cs11 << endl;
         cout << "CS: " << CS << endl;
-    } else{ 
+    }
+    else
+    {
         CS = 0;
         cout << "CS: " << CS << endl;
     }
@@ -130,11 +140,14 @@ double outCS(double plab, double pd)
 double bilinear_interpolation(double plab1, double plab2, double p1, double p2, double cs11, double cs12, double cs21, double cs22, double plab, double pdeut)
 {
     double logcs11 = (cs11 > 0) ? log10(cs11) : -100;
+    double logcs12 = (cs12 > 0) ? log10(cs12) : -100;
+    double logcs21 = (cs21 > 0) ? log10(cs21) : -100;
+    double logcs22 = (cs22 > 0) ? log10(cs22) : -100;
 
     double cs = (logcs11 * (log10(plab2) - log10(plab)) * (log10(p2) - log10(pdeut)) +
-                 log10(cs21) * (log10(plab) - log10(plab1)) * (log10(p2) - log10(pdeut)) +
-                 log10(cs12) * (log10(plab2) - log10(plab)) * (log10(pdeut) - log10(p1)) +
-                 log10(cs22) * (log10(plab) - log10(plab1)) * (log10(pdeut) - log10(p1))) /
+                 logcs21 * (log10(plab) - log10(plab1)) * (log10(p2) - log10(pdeut)) +
+                 logcs12 * (log10(plab2) - log10(plab)) * (log10(pdeut) - log10(p1)) +
+                 logcs22 * (log10(plab) - log10(plab1)) * (log10(pdeut) - log10(p1))) /
                 ((log10(plab2) - log10(plab1)) * (log10(p2) - log10(p1)));
 
     return pow(10., cs);
